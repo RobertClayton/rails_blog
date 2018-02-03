15783632b36a8597cd649335e3ad97dc9211c4d4
Robert Clayton <robbie.c.clayton@gmail.com>
2018-02-03 23:59:43 +0000

    saves created article to database

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,6 +3,13 @@ class ArticlesController < ApplicationController
   end
 
   def create
-    render plain: params[:article]
+    @article = Article.new(article_params)
+    @article.save
+    redirect_to @article
+  end
+
+  private
+  def article_params
+    params.require(:article).permit(:title, :text)
   end
 end
